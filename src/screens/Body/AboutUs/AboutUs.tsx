0d303a486b22351ca7b76de86e57ce35dc11a08a
Alan Schmidt <alu.schmidt@hotmail.com>
2023-04-21 11:45:11 -0400

    AboutUs style added

--- src/screens/Body/AboutUs/AboutUs.tsx
+++ src/screens/Body/AboutUs/AboutUs.tsx
@@ -1,3 +1,4 @@
+import { colors } from '@mui/material'
 import { Image } from 'mui-image'
 import aboutUsBody from '../../../images/AboutUsBody.png'
 
@@ -21,25 +22,38 @@ const AboutUs = () => (
           textAlign: 'justify',
         }}>
         Our Tennis Academy was born as a result of the passion that has always existed in
-        our family for this sport. Since our grandparents, tennis has been our inspiration
-        and motivation, and has served as the basis for our values of growth, optimism,
-        resilience, and the great seed that we have been cultivating over the years,
-        allowing our children to study in the United States on scholarships obtained
-        through tennis.
+        our family for this sport. Since our grandparents,{' '}
+        <span style={{ color: '#eb8611' }}>
+          tennis has been our inspiration and motivation
+        </span>
+        , and has served as the basis for our values of growth, optimism, resilience, and
+        the great seed that we have been cultivating over the years, allowing our children
+        to study in the United States on scholarships obtained through tennis.
         <p>
-          At our academy, children are trained in sports in an environment of friendship,
-          family, and support. Young people can prepare themselves, if they wish, for
-          tennis to be a bridge that allows them to obtain a scholarship for their
-          university studies in the United States, while adults can form a group of
-          friends who are united and distracted beyond tennis.
+          At our academy, children are trained in sports in an{' '}
+          <span style={{ color: '#1d70b7' }}>
+            environment of friendship, family, and support
+          </span>
+          . Young people can prepare themselves, if they wish, for tennis to be a bridge
+          that allows them to obtain a{' '}
+          <span style={{ color: '#39b3ad' }}>
+            scholarship for their university studies in the United States
+          </span>
+          , while adults can form a group of friends who are united and distracted beyond
+          tennis.
         </p>
         We provide personalized and professional training and conditioning for a healthy
         lifestyle, tailored to different age groups. Our coaching services are
         complemented by nutritional guidance to improve and enhance your performance. Our
         purpose is to inspire and motivate you so that through tennis as a game and
-        discipline, you achieve your best version, enhance your talents, challenge your
-        own limits, and improve day by day, giving your best on and off the court, as we
-        seek integral development of the person.
+        discipline,
+        <span className="violet-text" style={{ color: '#9a42b1' }}>
+          {' '}
+          you achieve your best version, enhance your talents, challenge your own limits,
+          and improve day by day
+        </span>
+        , giving your best on and off the court, as we seek integral development of the
+        person.
       </p>
     </div>
     {/*  <div className="stats--container" > */}
